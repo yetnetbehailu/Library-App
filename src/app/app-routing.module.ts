4f44e391bad795a629b157f48afa283ded4f52fa
yetnetbehailu <72866660+yetnetbehailu@users.noreply.github.com>
2023-08-08 23:55:18 +0200

    Installed bootstrap & created books component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:BooksComponent
+  },
+  {
+    path:'books',
+    component:BooksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
