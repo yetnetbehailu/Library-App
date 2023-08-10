7bb60177901aee8f0de718e4c171405fcb5cd2de
yetnetbehailu <72866660+yetnetbehailu@users.noreply.github.com>
2023-08-10 21:54:23 +0200

    Added create book routing & moved font imports to style.css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
+import { CreateBookComponent } from './books/create-book/create-book.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
   {
     path:'books',
     component:BooksComponent
+  },
+  {
+    path:'books/create',
+    component:CreateBookComponent
   }
 ];
 
