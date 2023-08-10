0b4828f40e7865e6ac9455930b544f508fe43e6d
yetnetbehailu <72866660+yetnetbehailu@users.noreply.github.com>
2023-08-10 22:10:06 +0200

    Added create-book component, template structure & styling. Included FormsModule for data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CreateBookComponent } from './books/create-book/create-book.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    CreateBookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
